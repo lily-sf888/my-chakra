f9ef74ed8904453413cb225051f7afb721385013
LilyV <lily.sf888@gmail.com>
2016-08-25 17:51:20 +0000

    fixed server

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 
 var app = express();
+
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+exports.app = app;
 
 console.log('listening');
