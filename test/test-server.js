77e7de7ff5ba766b89b8a3726fe9cbadc84f4393
LilyV <lily.sf888@gmail.com>
2016-08-26 16:49:17 +0000

    adding image folder

--- test/test-server.js
+++ test/test-server.js
@@ -9,13 +9,13 @@ var storage = server.storage;
 chai.use(chaiHttp);
 
 //add test to make sure we are hitting up the root url for the client and get a 200 status code and HTML
-describe('index page', function() {
+describe('signup page', function() {
     it('exists', function(done) {
         chai.request(app)
         .get('/')
         .end(function(err, res) {
             res.should.have.status(200);
-            res.should.be.html;
+            res.should.be.ejs;
             done();
         });
     });
